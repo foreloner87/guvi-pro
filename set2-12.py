68cf1f92c4e877f0532585871963ac6f419aa59e
foreloner87 <34986630+foreloner87@users.noreply.github.com>
2019-02-09 17:50:50 +0530

    Update set2-12.py

--- set2-12.py
+++ set2-12.py
@@ -3,26 +3,23 @@ def adv(x,y,l):
     for m in range(x,y+1):
         v=v+l[m]
     return v
-
-n=int(input())
-q=int(input())
+n,q=input().split()
+n,q=int(n),int(q)
 if(n>0):
-    l=[]
     r1=[]
     r2=[]
-    for i in range(n):
-        st=int(input())
-        l.append(st)
     if(n>1):
+        l=list(map(int,input().split()))
         for j in range(q):
-            s1=int(input())
-            s2=int(input())
+            s1,s2=input().split()
+            s1,s2=int(s1),int(s2)
             r1.append(s1)
             r2.append(s2)
         for k in range(q):
             print( adv((r1[k]-1),(r2[k]-1),l))
     elif(n==1):
+        s=input()
         for i in range(q):
-            print(st)
+            print(s)
 else:
     print("Invalid input")
