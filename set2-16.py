de722708b758b9417c8601569b41ff706685427a
foreloner87 <34986630+foreloner87@users.noreply.github.com>
2019-02-09 23:35:05 +0530

    Update set2-16.py

--- set2-16.py
+++ set2-16.py
@@ -1,15 +1,13 @@
 n=int(input())
-f,c,t=0,0,0
-l=list(map(int,input().split()))
-for i in range(max(l)):
-    for j in l:
-        if(j>=i+1):
-            f=f+1
-            if (i+1 not in l):
-                t=t+1
-                c=f
-                break
-if(t==0):
-    print(f)
-elif(t!=0):
+if(n>0):
+    f=[]
+    c=0
+    l=list(map(int,input().split()))
+    for i in range(1,max(l)+1):
+        if(i in l):
+            f.append(l.count(i))
+    for j in range(0,len(f)):
+        c=c+(f[j]*(j+1))
     print(c)
+else:
+    print("Invalid input")
